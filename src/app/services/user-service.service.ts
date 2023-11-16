343e2ed727f80121a1b7ef9bc233f39cffc605bb
sumayya <sumayya.kareem6@gmail.com>
2023-11-16 09:53:39 +0530

    updated urls

--- src/app/services/user-service.service.ts
+++ src/app/services/user-service.service.ts
@@ -52,23 +52,23 @@ export class UserServiceService {
  
   getUsersList() {
     console.log("inside getemployers")
-    return this.http.get<any>("https://shopnow-t9zl.onrender.com/user/getUsers");
+    return this.http.get<any>("https://shopnowapi-ydrz.onrender.com/user/getUsers");
   }
   getCartItems() {
-    return this.http.get<any>("https://shopnow-t9zl.onrender.com/user/getCartItems");
+    return this.http.get<any>("https://shopnowapi-ydrz.onrender.com/user/getCartItems");
   }
   getProductById(id:any):Observable<any> {
-   return this.http.get<any>("https://shopnow-t9zl.onrender.com/user/getProduct/"+id);
+   return this.http.get<any>("https://shopnowapi-ydrz.onrender.com/user/getProduct/"+id);
   }
   getUserById(id:any) {
-    return this.http.get<any>("https://shopnow-t9zl.onrender.com/user/getUserById/"+id);
+    return this.http.get<any>("https://shopnowapi-ydrz.onrender.com/user/getUserById/"+id);
   }
   getCart(id:any) {
-    return this.http.get<any>("https://shopnow-t9zl.onrender.com/user/getCart/"+id);
+    return this.http.get<any>("https://shopnowapi-ydrz.onrender.com/user/getCart/"+id);
   }
   getCartItemsById(userId:any):Observable<any> {
     console.log(userId);
-    return this.http.get<any>("https://shopnow-t9zl.onrender.com/user/getCartItemsById/"+userId);
+    return this.http.get<any>("https://shopnowapi-ydrz.onrender.com/user/getCartItemsById/"+userId);
   }
   
   
@@ -76,24 +76,24 @@ export class UserServiceService {
   getUserByName(name:any){
     console.log("inside getuser");
     console.log(name);
-    return this.http.get<any>("https://shopnow-t9zl.onrender.com/user/getUserByName/"+name);
+    return this.http.get<any>("https://shopnowapi-ydrz.onrender.com/user/getUserByName/"+name);
   }
 
 
   getUserByEmail(email:any){
     console.log(email)
-    return this.http.get<any>("https://shopnow-t9zl.onrender.com/user/getUserByemail/"+email);
+    return this.http.get<any>("https://shopnowapi-ydrz.onrender.com/user/getUserByemail/"+email);
   }
  
   
   userSignup(user: any) {
-    return this.http.post<any>("https://shopnow-t9zl.onrender.com/user/insertUser",{"user":user}).subscribe(data=>{console.log(data)});
+    return this.http.post<any>("https://shopnowapi-ydrz.onrender.com/user/insertUser",{"user":user}).subscribe(data=>{console.log(data)});
    }
 
    loginUser(user:any){
     alert("inside auth service");
     
-     return this.http.post<any>("https://shopnow-t9zl.onrender.com/user/usersignin",{"user":user});
+     return this.http.post<any>("https://shopnowapi-ydrz.onrender.com/user/usersignin",{"user":user});
   }
 
 
@@ -103,7 +103,7 @@ export class UserServiceService {
 
    addToCart(product:any,id:any) {
     console.log("got product quantity",product)
-    return this.http.post<any>("https://shopnow-t9zl.onrender.com/user/addToCart/"+id,{"product":product}).pipe(tap(()=>{
+    return this.http.post<any>("https://shopnowapi-ydrz.onrender.com/user/addToCart/"+id,{"product":product}).pipe(tap(()=>{
       return this._refreshrequired$.next();
     })).subscribe(data=>{console.log(data)});
 
@@ -112,28 +112,28 @@ export class UserServiceService {
     // let parameters = JSON.stringify({ 'totalPrice': tPrice, 'totalCount': tCount }); 
      console.log("parameters are",cartItems);
    
-    return this.http.post<any>("https://shopnow-t9zl.onrender.com/user/addToBag/"+tPrice+'/'+tCount+'/'+uName+'/'+uAddress,{"cartItems":cartItems}).subscribe(data=>{console.log(data)});
+    return this.http.post<any>("https://shopnowapi-ydrz.onrender.com/user/addToBag/"+tPrice+'/'+tCount+'/'+uName+'/'+uAddress,{"cartItems":cartItems}).subscribe(data=>{console.log(data)});
   }
    removeCartItem(id: any) {
-   return this.http.delete<any>("https://shopnow-t9zl.onrender.com/user/removeFromCart/"+id).pipe(tap(()=>{
+   return this.http.delete<any>("https://shopnowapi-ydrz.onrender.com/user/removeFromCart/"+id).pipe(tap(()=>{
     return this._refreshrequired$.next();
   }));
   
   }
   increasequantity(product:any) {
    
-   return this.http.put<any>("https://shopnow-t9zl.onrender.com/user/increasequantity/",product).pipe(tap(()=>{
+   return this.http.put<any>("https://shopnowapi-ydrz.onrender.com/user/increasequantity/",product).pipe(tap(()=>{
     return this._refreshrequired$.next();
   })).subscribe(data=>{console.log(data)});
   }
   increaseproductquantity(cartItem:any,quantity:any) {
    
-    return this.http.put<any>("https://shopnow-t9zl.onrender.com/user/increasequantity/"+quantity,cartItem).pipe(tap(()=>{
+    return this.http.put<any>("https://shopnowapi-ydrz.onrender.com/user/increasequantity/"+quantity,cartItem).pipe(tap(()=>{
      return this._refreshrequired$.next();
    })).subscribe(data=>{console.log(data)});
    }
   decreasequantity(product:any){
-    return this.http.put<any>("https://shopnow-t9zl.onrender.com/user/decreasequantity/",product).pipe(tap(()=>{
+    return this.http.put<any>("https://shopnowapi-ydrz.onrender.com/user/decreasequantity/",product).pipe(tap(()=>{
       return this._refreshrequired$.next();
     })).subscribe(data=>{console.log(data)});
   }
@@ -144,13 +144,13 @@ export class UserServiceService {
     // const userId=localStorage.getItem("editUserId");
     // console.log('job update')
   
-    return this.http.put("https://shopnow-t9zl.onrender.com/user/updateuser/"+id,{"user":user}).subscribe(data=>{console.log("data is"+data)});
+    return this.http.put("https://shopnowapi-ydrz.onrender.com/user/updateuser/"+id,{"user":user}).subscribe(data=>{console.log("data is"+data)});
   }
   updateuser(mylocation:any,user:any){
     
    
     console.log(mylocation);
-    return this.http.put("https://shopnow-t9zl.onrender.com/user/updatedata/"+mylocation,{"user":user}).subscribe(data=>{console.log("data is"+data)});
+    return this.http.put("https://shopnowapi-ydrz.onrender.com/user/updatedata/"+mylocation,{"user":user}).subscribe(data=>{console.log("data is"+data)});
   }
 
 }
